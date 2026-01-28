375344f395ced939de728f156f65d8941a17b31e
clementchewzhixiang-cyber <clementchewzhixiang@gmail.com>
2026-01-28 11:24:36 +0800

    Add files via upload

---
+++ Campus_Location_Helper/Location.java
@@ -0,0 +1,30 @@
+package Campus_Location_Helper;
+
+public class Location {
+
+	String name, String purpose ;
+	public static void main Dairy (String n,String p)
+	
+}
+	
+	{
+		n=name
+		p=purpose
+		
+	public static void main displayInfo
+	{
+			System.out.println("Name:")
+			system.oput.println("Purpose:"+purpose);
+			
+	public static voiid main()
+	{
+		building d =new Building("Library","Study Area")
+		building e =new Building ("Canteen”, “Dining Area")
+		
+			d.show();
+			e.displayInfo();
+	
+
+	}
+		
+}
